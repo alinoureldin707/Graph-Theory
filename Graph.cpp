bb5fa9b7db467d595fd8ade3f16c751da14c00a9
ali noureldin <alinoureldin707@gmail.com>
2021-10-21 20:02:12 +0300

    initializing graph

--- Graph.cpp
+++ Graph.cpp
@@ -3,6 +3,21 @@
 using namespace std;
 
 class Graph{
-    int V,E;
-    int** Adj;
+    public:
+        int V,E;
+        int** Adj;
+    Graph(int V,int E)
+    {
+        this->V = V;
+        this->E = E;
+        this->Adj = (int**) malloc(sizeof(int*) * this->V);
+        for(int i = 0; i < this->V;i++)
+        {
+            this->Adj[i] = (int*) malloc(sizeof(int) * this->V);
+            for(int j = 0; j < this->V;j++)
+                this->Adj[i][j] = 0;
+        }
+    }
+    
 };
+
